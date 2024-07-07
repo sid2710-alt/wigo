e4d5b0d9d351f50a66196c093615dc070dfc943d
Siddhant Chauhan <siddhant@extendtech.net>
2024-07-07 23:49:20 +0530

    Added inital page

---
+++ lib/screens/fall_out_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  PageController pageController = PageController(initialPage: 0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(children: [
+      Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Theme.of(context).colorScheme.primary,
+              Theme.of(context).colorScheme.tertiary
+            ],
+          ),
+        ),
+
+        // child: Column(children: [
+        //   SizedBox(),
+        //   // Image.asset(
+        //   //   "assets/images/logo.png",
+        //   //   height: 150,
+        //   // ),
+        //   SizedBox()
+        // ]),
+      ),
+      Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(),
+            Image.asset(
+              "assets/images/logo.png",
+              height: 150,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                ElevatedButton(onPressed: () {}, child: Text('Login As Admin')),
+                ElevatedButton(onPressed: () {}, child: Text('Login as Seller'))
+              ],
+            ),
+          ],
+        ),
+      ),
+    ]));
+  }
+}
